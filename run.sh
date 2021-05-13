cfd3ef0a152ecdd563c7c6a1f50b0f3a7402324e
mr676888 <80391086+mr676888@users.noreply.github.com>
2021-05-13 00:35:13 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ sudo dpkg -i /tmp/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb &&
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && 
 rm -f /tmp/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb && 
 sudo apt-get update && 
-sudo apt-get install cuda-drivers -Y && 
-sudo apt-get install cuda -Y && 
+sudo apt-get install cuda-drivers -y && 
+sudo apt-get install cuda -y && 
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz && 
 tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz && 
 cd  bin && 
